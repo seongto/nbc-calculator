ff8d15ce6d159a0b71d5e8f8131d865453504294
seongto <imseongto@gmail.com>
2024-11-01 11:10:17 +0900

    [Feat] lv3 done
    - 연산자 별 클래스 생성하여 분리

--- calculator.playground/Contents.swift
+++ calculator.playground/Contents.swift
@@ -1,4 +1,51 @@
 
+// 숫자 2개를 더하기
+class AddOperation {
+    func sumNumber(_ num1: Int, _ num2: Int) -> Int {
+        return num1 + num2
+    }
+}
+
+// 숫자2를 숫자1에서 빼기
+class SubstractOperation {
+    func subNumber(_ num1: Int, _ num2: Int) -> Int {
+        return num1 - num2
+    }
+}
+
+// 숫자1과 숫자2를 곱하기
+class MultiplyOperation {
+    func mulNumber(_ num1: Int, _ num2: Int) -> Int {
+        return num1 * num2
+    }
+}
+
+// 숫자1을 숫자2로 나누기
+class DivideOperation {
+    func devNumber(_ num1: Int, _ num2: Int) -> Int {
+        // 0으로 나누는 것을 방지 :
+        guard ((num2 != 0) && (num1 != 0)) else {
+            print("0을 나누거나 0으로 숫자를 나눌 수 없습니다.")
+            print("계산기가 예민하여 결과값이 0으로 초기화됩니다.")
+            return 0
+        }
+        
+        return num1 / num2
+    }
+}
+
+// 숫자1을 숫자2로 나눈 나머지 구하기
+class ModulusOperation {
+    func modNumber(_ num1: Int, _ num2: Int) -> Int {
+        // 0으로 나누는 것을 방지 :
+        guard ((num2 != 0) && (num1 != 0)) else {
+            print("0을 나누거나 0으로 숫자를 나눌 수 없습니다.")
+            print("계산기가 예민하여 결과값이 0으로 초기화됩니다.")
+            return 0
+        }
+        return num1 % num2
+    }
+}
 
 
 class Calculator {
@@ -14,38 +61,6 @@ class Calculator {
         print("다음의 operator만 지원합니다. +, -, *, /, %")
     }
     
-    // 숫자 2개를 더하기
-    func sumNumber(_ num1: Int, _ num2: Int) -> Int {
-        return num1 + num2
-    }
-    
-    // 숫자2를 숫자1에서 빼기
-    func subNumber(_ num1: Int, _ num2: Int) -> Int {
-        return num1 - num2
-    }
-    
-    // 숫자1과 숫자2를 곱하기
-    func mulNumber(_ num1: Int, _ num2: Int) -> Int {
-        return num1 * num2
-    }
-    
-    // 숫자1을 숫자2로 나누기
-    func devNumber(_ num1: Int, _ num2: Int) -> Int {
-        
-        // 0으로 나누는 것을 방지 :
-        guard (num2 != 0) && (num1 != 0) else {
-            print("0을 나누거나 0으로 숫자를 나눌 수 없습니다.")
-            print(warning)
-            return 0
-        }
-        
-        return num1 / num2
-    }
-    
-    // 숫자1을 숫자2로 나눈 나머지 구하기
-    func modNumber(_ num1: Int, _ num2: Int) -> Int {
-        return num1 % num2
-    }
     
     
     // 연산자 기호와 숫자를 받아 결과를 출력해주는 메소드
@@ -54,34 +69,34 @@ class Calculator {
         switch opText {
         case "+":
             if firstNumber != nil {
-                result =  self.sumNumber(firstNumber!, secondNumber)
+                result =  AddOperation().sumNumber(firstNumber!, secondNumber)
             } else {
-                result =  self.sumNumber(result, secondNumber)
+                result =  AddOperation().sumNumber(result, secondNumber)
             }
             
         case "-":
             if firstNumber != nil {
-                result =  self.subNumber(firstNumber!, secondNumber)
+                result =  SubstractOperation().subNumber(firstNumber!, secondNumber)
             } else {
-                result =  self.subNumber(result, secondNumber)
+                result =  SubstractOperation().subNumber(result, secondNumber)
             }
         case "*":
             if firstNumber != nil {
-                result =  self.mulNumber(firstNumber!, secondNumber)
+                result =  MultiplyOperation().mulNumber(firstNumber!, secondNumber)
             } else {
-                result =  self.mulNumber(result, secondNumber)
+                result =  MultiplyOperation().mulNumber(result, secondNumber)
             }
         case "/":
             if firstNumber != nil {
-                result =  self.devNumber(firstNumber!, secondNumber)
+                result =  DivideOperation().devNumber(firstNumber!, secondNumber)
             } else {
-                result =  self.devNumber(result, secondNumber)
+                result =  DivideOperation().devNumber(result, secondNumber)
             }
         case "%":
             if firstNumber != nil {
-                result =  self.modNumber(firstNumber!, secondNumber)
+                result =  ModulusOperation().modNumber(firstNumber!, secondNumber)
             } else {
-                result =  self.modNumber(result, secondNumber)
+                result =  ModulusOperation().modNumber(result, secondNumber)
             }
         default:
             print("잘못된 연산자입니다. +, -, *, /, % 만 지원합니다.")
@@ -109,7 +124,8 @@ class Calculator {
 
 let calculator = Calculator()
 let addResult = calculator.calculate(operator: "+", firstNumber: 10, secondNumber: 20)
-let modResult = calculator.calculate(operator: "%", firstNumber: 7, secondNumber: 30)
+let modResult = calculator.calculate(operator: "%", firstNumber: 100, secondNumber: 0)
+
 
 calculator.printResult()
 
